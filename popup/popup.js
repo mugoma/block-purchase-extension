7adccdc7605cb13abe2c1fe91fc4e3705d75f899
Okomba Mugoma <mugoma99@gmail.com>
2024-03-02 22:06:34 -0800

    Log changes!

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elem = document.querySelector('#add-timer-btn');
-    elem.addEventListener("click", () => {
-        var url = window.location.toString()
-        add_url_to_storage(url)
-    })
 
-})
+
 function add_url_to_storage(url, current_time) {
     chrome.storage.local.get(url).then((value) => {
-        if (value == null) {
-            chrome.storage.local.set({url: current_time}).then(()=>{console.log("added to the best o my ability")});
-            
-        };
-    })
-    // const time_added = chrome.storage.local.getItem(url);
-    // var response = "Object already in countdown";
-    // if (time_added == null) {
-    //     window.localStorage.setItem(url, current_time);
-    //     response = "Object added to countdown"
-    // };
-    // console.log("added time")
+        if (!value) {
+            chrome.storage.local.set({ url: current_time }).then(() => { console.log("added to the best o my ability") });
 
+        };
+        document.querySelector('#add-timer-btn').remove()
+        document.querySelector('#popup-text').innerHTML="Added to timer!"
+
+    })
 }
 
+function countdown(time){
+    //TODO: Add countdown
+}
+document.querySelector('#add-timer-btn').addEventListener("click", () => {
+    var url = window.location.toString()
+    add_url_to_storage(url)
+})
