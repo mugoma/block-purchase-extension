5bbb4716120b70d43b6e3ac46c59d33f65b34f47
Okomba Mugoma <mugoma99@gmail.com>
2024-10-14 15:50:43 -0700

    Add TODO;

--- scripts/background/background.js
+++ scripts/background/background.js
@@ -1,4 +1,4 @@
-import { getOrSetTime, resetTimeInStorage } from "./utils_chrome_api.js";
+import { getOrSetTime, resetTimeInStorage, sendMessageToContentScript } from "./utils_chrome_api.js";
 
 const SET_TIMER_ACTION = 'set-timer';
 const RESET_TIMER_ACTION = 'reset-timer';
@@ -8,23 +8,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === SET_TIMER_ACTION) {
         const url = message.url
         // Add timer if none. Avoid resetting the timer if action is initiated by the page CTA
-        getOrSetTime(url).then((time) => { sendResponse(time) }).then(()=>{
-            // Send to content script
-
-        });
+        getOrSetTime(url).then((endTime) => { sendResponse(endTime); return endTime })
+        // .then((endTime) => {
+        //     // Send to content script
+        //     //sendMessageToContentScript(SET_TIMER_ACTION, endTime);
+        // });
 
     } else if (message.action === RESET_TIMER_ACTION) {
         const url = message.url
-        resetTimeInStorage(url).then((time) => { sendResponse(time) }).then(()=>{
-            // Send to content script
+        resetTimeInStorage(url).then((time) => { sendResponse(time) }).then(() => {
+            // TODO: Send to content script
         });
     }
     else if (message.action === DELETE_TIMER_ACTION) {
         const url = message.url;
         chrome.storage.local.remove(url);
-        sendResponse("Deleted").then(()=>{
-            // Send to content script
-        });
+        sendResponse("Deleted")
+        //TODO: Send to content script
+
     }
     // Run asynchronously
     return true;
