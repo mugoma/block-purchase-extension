5d2fdf06a582b0202f2f917d8b07b5633fc400b4
Okomba Mugoma <mugoma99@gmail.com>
2024-10-28 11:39:45 -0700

    Format code;

--- scripts/pages/options.js
+++ scripts/pages/options.js
@@ -1,8 +1,7 @@
 const saveOptions = () => {
     const timer = document.getElementById('timer').checked;
 
-    chrome.storage.local.set(
-        { timer: timer },
+    chrome.storage.local.set({ timer: timer }).then(
         () => {
             // Update status to let user know options were saved.
             const status = document.getElementById('status');
@@ -15,8 +14,7 @@ const saveOptions = () => {
 };
 
 const restoreOptions = () => {
-    chrome.storage.local.get(
-        { timer: true },
+    chrome.storage.local.get({ timer: true }).then(
         (items) => {
             document.getElementById('timer').checked = items.timer;
         }
